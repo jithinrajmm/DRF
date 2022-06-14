b95a3e1a8a510f5d97164b1098dfed79bf724d27
jithin <jithinbuasc@gmail.com>
2022-06-14 07:52:51 +0530

    added the Serilzer.serilizerModelFeild for changing the original method name to calling name, in serilazer.py

--- backend/product/serializer.py
+++ backend/product/serializer.py
@@ -4,12 +4,17 @@ from product.models import Product
 
 
 class ProductSerilaizer(serializers.ModelSerializer):
-    
+    discount = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Product
         fields = [
             'id',
             'content',
             'title',
-          'final_discount'
+            'discount'
         ]
+        
+    def get_discount(self,selfofproduct):
+        return selfofproduct.final_discount()
+        # call that function which is in the models
+        # i got erro because of i forgot to call the function
